6f8c2a371937348a71fc450cb31c6534ad50e0af
Hobokian <joedahobo13@gmail.com>
2025-12-03 21:09:41 -0500

    d3 - start - wrong already... order matter from the get go

---
+++ D3/p1.py
@@ -0,0 +1,17 @@
+import re
+
+input = open("./D3/input.txt", "r").readlines()
+#print(input)
+
+for battery_bank in input:
+    battery_bank = battery_bank.strip()
+    print(battery_bank)
+    battery_bank_list = list(battery_bank)
+    battery_bank_list_temp = list(battery_bank)
+    # FIND - the 2 largest digits in the string in any order
+    NUMBER_1 = max(battery_bank_list_temp)
+    print(NUMBER_1)
+    battery_bank_list_temp.remove(NUMBER_1)
+    NUMBER_2 = max(battery_bank_list_temp)
+    print(NUMBER_2)
+    # THEN - put (NOT ADD) them together in the original order found in the string
