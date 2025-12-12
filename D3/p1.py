0ec7e50a1dd2c96f0c684307bcaf79db89e35286
Hobokian <joedahobo13@gmail.com>
2025-12-11 20:07:49 -0500

    d3 - p1 working!

--- D3/p1.py
+++ D3/p1.py
@@ -1,6 +1,7 @@
 input = open("./D3/input.txt", "r").readlines()
 #print(input)
 
+total_number = 0
 
 for battery_bank in input:
     battery_bank = battery_bank.strip()
@@ -10,22 +11,27 @@ for battery_bank in input:
     first_number_index = -1
     for index in range(len(battery_bank)-1):
         #print(battery_bank[index])
-        if battery_bank[index] > battery_bank[index+1] and str(first_number) < battery_bank[index]:
+        if str(first_number) < battery_bank[index]:
             first_number = battery_bank[index]
             first_number_index = index
     print("First number: " + str(first_number))
-    print("First number index: " + str(first_number_index))
+    #print("First number index: " + str(first_number_index))
     # LOOP 2 - from largest number index +1 find the next largest number
     second_number = -1
     for indextwo in range(first_number_index+1, len(battery_bank)):
-        print(battery_bank[first_number_index+indextwo])
+        #print(battery_bank[indextwo])
         try:
-            if battery_bank[first_number_index+indextwo] > battery_bank[first_number_index+indextwo+1] and str(second_number) < battery_bank[first_number_index+indextwo]:
-                second_number = battery_bank[first_number_index+indextwo]
+            if str(second_number) < battery_bank[indextwo]:
+                second_number = battery_bank[indextwo]
         except IndexError:
             continue
     print("Second number: " + second_number)
     print("Battery is: " + first_number + second_number)
+    battery_number = str(first_number) + str(second_number)
+    print("BN: " + str(battery_number))
+    total_number += int(battery_number)
+print(total_number)
+
 
 
 # for battery_bank in input:
