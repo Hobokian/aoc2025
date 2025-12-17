d5ed46476f6ac59cee9da823b5a9b72663cb2286
Hobokian <joedahobo13@gmail.com>
2025-12-16 21:58:51 -0500

    D8 - p1 - broken code but something maybe..?

--- D8/p1.py
+++ D8/p1.py
@@ -1,55 +1,51 @@
-import numpy as np
+import math
 
-input = np.loadtxt('./D8/input.txt', delimiter=',')
+# Taken from google AI directly :)
+def euclidean_distance(point1, point2):
+    distance_squared = 0
+    for i in range(len(point1)):
+        distance_squared += (point2[i] - point1[i]) ** 2
+    return math.sqrt(distance_squared)
+
+
+
+input = open("./D8/input.txt", "r").readlines()
 #print(input)
 
-prev_point = np.array([])
-
-circuit_list = []
-
-for item in (input):
-    print(item)
-
-    distance = None
-    index = None
-    for j in range(len(input)-1):
+# Make input useable
+input_list = []
+for i in input:
+    #print(i)
+    int_list = i.split(',')
+    temp_list = []
+    for j in int_list:
         #print(j)
-        if distance == None:
-            distance = np.linalg.norm(item - input[j+1])
-            index = j+1
-        else:
-            if distance > np.linalg.norm(item - input[j+1]):
-                distance = np.linalg.norm(item - input[j+1])
-                index = j+1
-    print(distance)
-    print(index)
-#     circuit_list.append([item, input[index]])
-# print(circuit_list)
+        temp_list.append(int(j))
+    input_list.append(temp_list)
+#print(input_list)
 
+# point1 = input_list[0]
+# point2 = input_list[19]
+distance_list = []
+for item in input_list:
+    for index in range(len(input_list)-1):
+        # Check every item in the list against every other item in the list
+        if item == input_list[index]:
+            continue
+        distance = euclidean_distance(item, input_list[index])
+        #print(distance)
+        distance_list.append([item, input_list[index], distance])
+    #print(distance_list)
+    #distance_list.sort()
+print(distance_list)
 
-
-
-
-
-#     if prev_point.size == 0:
-#         prev_point = item
-#     else:
-#         curr_point = item
-#         if prev_distance == None:
-#             prev_distance = np.linalg.norm(prev_point - curr_point)
-#         else:
-#             if np.linalg.norm(prev_point - curr_point) < prev_distance:
-#                 prev_distance = np.linalg.norm(prev_point - curr_point)
-
-# print(prev_point)
-# print(prev_distance)
-
-
-
-
-# point1 = np.array([162,817,812])
-# point2 = np.array([346,949,466])
-
-# distance = np.linalg.norm(point1 - point2)
-# print(f"Euclidean distance: {distance}")
-# # Output: Euclidean distance: 5.196152422706632
+## Thoughts on next steps...
+# Loop through distance_list and count the number of duplicates?
+seen = set()
+duplicate_count = 0
+for item in distance_list:
+    if item in seen:
+        duplicate_count +=1
+    else:
+        seen.add(item)
+print(duplicate_count)
